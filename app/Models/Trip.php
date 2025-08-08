761e975052d934f0138193ee932c39a8276c0d0c
Yos Rio <yosrio71@gmail.com>
2025-08-08 19:51:12 +0700

    [TRAVELNEST-6] - Adjust model relation function

--- app/Models/Trip.php
+++ app/Models/Trip.php
@@ -48,5 +48,85 @@ class Trip extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    /**
+     * The itineraries that belong to the Trip
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function itineraries()
+    {
+        return $this->hasMany(Itinerary::class);
+    }
+
+    /**
+     * Get the budgets that belong to the Trip
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function budgets()
+    {
+        return $this->hasMany(Budget::class);
+    }
+
+    /**
+     * Get the expenses that belong to the Trip.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasManyThrough
+     */
+    public function expenses()
+    {
+        return $this->hasManyThrough(Expense::class, Budget::class);
+    }
+
+    /**
+     * The checklists that belong to the Trip
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function checklists()
+    {
+        return $this->hasMany(Checklist::class);
+    }
+
+    /**
+     * Get the documents that belong to the Trip
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function documents()
+    {
+        return $this->hasMany(Document::class);
+    }
+
+    /**
+     * The photos that belong to the Trip
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function photos()
+    {
+        return $this->hasMany(Photo::class);
+    }
+
+    /**
+     * The reviews that belong to the Trip
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    /**
+     * Get the shared tokens that belong to the Trip
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function sharedTokens()
+    {
+        return $this->hasMany(SharedToken::class);
+    }
 }
 

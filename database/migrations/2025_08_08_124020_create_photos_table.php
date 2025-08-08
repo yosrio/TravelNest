0f9d8075cab89debc2e5f48da12ca4e6ca368894
Yos Rio <yosrio71@gmail.com>
2025-08-08 19:41:55 +0700

    [TRAVELNEST-13] - Migrations & Models Photos

---
+++ database/migrations/2025_08_08_124020_create_photos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('trip_id');
+            $table->text('caption')->nullable();
+            $table->string('image_path');
+            $table->integer('taken_on_day')->nullable();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'))->useCurrentOnUpdate();
+
+            $table->foreign('trip_id')->references('id')->on('trips')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('photos');
+    }
+};

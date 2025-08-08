ea1a4516e102e57515456f91bc55f8e80ff2fad8
Yos Rio <yosrio71@gmail.com>
2025-08-08 19:32:36 +0700

    [TRAVELNEST-9] - Migrations & Models Budgets

---
+++ database/migrations/2025_08_08_123032_create_budgets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('budgets', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('trip_id');
+            $table->enum('category', ['lodging', 'food', 'transport', 'entertainment', 'misc']);
+            $table->decimal('estimated_amount', 12, 2)->default(0.00);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'))->useCurrentOnUpdate();
+
+            $table->foreign('trip_id')->references('id')->on('trips')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('budgets');
+    }
+};

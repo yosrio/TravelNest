2d1f5bfb52215f41f01aab67eb02c8b574431467
Yos Rio <yosrio71@gmail.com>
2025-08-08 20:07:44 +0700

    [TRAVELNEST-17] - Create Api Register User

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\JsonResponse;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request): JsonResponse
+    {
+        $user = User::create([
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => Hash::make($request->password),
+            'photo'    => null,
+            'bio'      => null,
+        ]);
+
+        return response()->json([
+            'status'  => 'success',
+            'message' => 'Registration successful',
+            'data'    => [
+                'id'    => $user->id,
+                'name'  => $user->name,
+                'email' => $user->email,
+            ]
+        ], 201);
+    }
+}

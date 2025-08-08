3f6888cd30e1d203948511fda716e147b49b8745
Yos Rio <yosrio71@gmail.com>
2025-08-08 19:34:49 +0700

    [TRAVELNEST-10] - Migrations & Models Expenses

---
+++ app/Models/Expense.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class Expense extends Model
+{
+    use HasUuids;
+
+    protected $table = 'expenses';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'budget_id',
+        'description',
+        'amount',
+        'spent_at',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'amount' => 'decimal:2',
+        'spent_at' => 'date',
+    ];
+
+    /**
+     * Get the budget that owns the Expense
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function budget()
+    {
+        return $this->belongsTo(Budget::class);
+    }
+}
---
+++ database/migrations/2025_08_08_123322_create_expenses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('budget_id');
+            $table->string('description');
+            $table->decimal('amount', 12, 2)->default(0.00);
+            $table->date('spent_at');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'))->useCurrentOnUpdate();
+
+            $table->foreign('budget_id')->references('id')->on('budgets')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expenses');
+    }
+};

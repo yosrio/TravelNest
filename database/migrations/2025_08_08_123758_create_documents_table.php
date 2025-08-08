4f0d7bf5267c8526d469dbe348efa2b003f559e4
Yos Rio <yosrio71@gmail.com>
2025-08-08 19:39:20 +0700

    [TRAVELNEST-12] - Migrations & Models Documents

---
+++ database/migrations/2025_08_08_123758_create_documents_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('trip_id');
+            $table->string('title');
+            $table->string('file_path');
+            $table->enum('file_type', ['image', 'pdf', 'note']);
+            $table->text('description')->nullable();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'))->useCurrentOnUpdate();
+
+            $table->foreign('trip_id')->references('id')->on('trips')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documents');
+    }
+};

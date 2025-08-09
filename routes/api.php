d2dda231b62e7952b590e1bde6e2d80b74abb462
Yos Rio <yosrio71@gmail.com>
2025-08-09 21:27:25 +0700

    Revert wrong push code

--- routes/api.php
+++ routes/api.php
@@ -5,13 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Rest\AuthController;
 use App\Http\Controllers\Rest\ProfileController;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/password/forgot', [AuthController::class, 'forgotPassword']);
-Route::post('/password/reset', [AuthController::class, 'resetPassword']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/password/forgot', [AuthController::class, 'forgotPassword']);
+Route::post('/auth/password/reset', [AuthController::class, 'resetPassword']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
 
     Route::get('/user/profile', [ProfileController::class, 'show']);
     Route::put('/user/profile', [ProfileController::class, 'update']);

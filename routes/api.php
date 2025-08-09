a4f6ac102e0f1825912bc23f45819ca67ebfd8c2
Yos Rio <yosrio71@gmail.com>
2025-08-09 14:18:40 +0700

    Adjustment Path Controller

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\ValidationException;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Rest\AuthController;
 
-Route::post('/auth/register', [AuthController::class, 'register']);
-Route::post('/auth/login', [AuthController::class, 'login']);
-Route::post('/auth/password/forgot', [AuthController::class, 'forgotPassword']);
-Route::post('/auth/password/reset', [AuthController::class, 'resetPassword']);
-Route::middleware('auth:sanctum')->post('/auth/logout', [AuthController::class, 'logout']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/password/forgot', [AuthController::class, 'forgotPassword']);
+Route::post('/password/reset', [AuthController::class, 'resetPassword']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::put('/user/profile', [AuthController::class, 'editProfile']);
+});
